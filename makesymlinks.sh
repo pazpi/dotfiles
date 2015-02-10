a919fe289957d3b14d8a99102c2970988298c7aa
Davide <pasettodavide@gmail.com>
2015-02-10 19:39:00 +0100

    added conky dotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc zshrc Xresources"    # list of files/folders to symlink in homedir
+files="bashrc vimrc zshrc Xresources conkyrc conkyMPD conkyCLOCK"    # list of files/folders to symlink in homedir
 
 ##########
 
