e6d7a543632e75a0869f02a340be0f3e0b7aaf4f
Davide <pasettodavide@gmail.com>
2015-03-15 13:55:13 +0100

    Add some comment Explain what this script do

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -1,2 +1,4 @@
-ssh noscreen mkdir -p /mnt/500omega/$(date -I)
+#Backup script: crete a directory whith date and copy all file listed, except those in the file rsync-exclude.txt
+
+ssh noscreen mkdir -p /mnt/500omega/LenovoManjaro$(date -I)
 sudo rsync -aAXvP --exclude-from=$HOME/dotfiles/rsync-exclude.txt /* -e ssh noscreen:/mnt/500omega/LenovoManjaro$(date -I)
