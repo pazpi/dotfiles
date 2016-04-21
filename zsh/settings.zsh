49443a9d62606ad1f37c36441a38abd157155b32
pazpi <pasettodavide@gmail.com>
2016-04-21 11:42:29 +0200

    Fix things and small changes

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -47,8 +47,8 @@ bindkey "^R" history-incremental-search-backward
 # print previous command but only the first nth arguments
 # Alt+1, Alt+2 ...etc
 bindkey -s '\e1' "!:0 \t"
-bindkey -s '\e2' "!:0-1 \t"
-bindkey -s '\e3' "!:0-2 \t"
-bindkey -s '\e4' "!:0-3 \t"
-bindkey -s '\e5' "!:0-4 \t"
+bindkey -s '\e2' "!:1-1 \t"
+bindkey -s '\e3' "!:2-2 \t"
+bindkey -s '\e4' "!:3-3 \t"
+bindkey -s '\e5' "!:4-4 \t"
 bindkey -s '\e`' "!:0- \t"     # all but the last word
