94d0e909ce01d0f0af654abe48fbf606d64d4335
pazpi <pasettodavide@gmail.com>
2016-05-09 12:30:55 +0200

    Clean old file

--- rsync-backup.sh
+++
@@ -1,4 +0,0 @@
-#Backup script: crete a directory whith date and copy all file listed, except those in the file rsync-exclude.txt
-
-ssh pazpi-noscreen mkdir -p /mnt/500omega/Lenovo$(uname --kernel-release)$(date -I)
-rsync -aAXvP --exclude-from=$HOME/Projects/dotfiles/rsync-exclude.txt /* -e ssh pazpi-noscreen:/mnt/500omega/Lenovo$(uname --kernel-release)$(date -I)
