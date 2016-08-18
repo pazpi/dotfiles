0a1019ccda15111a7a38152342f0bf28f93dff97
pazpi <pasettodavide@gmail.com>
2016-08-18 18:47:05 +0200

    Change name of screen

--- bin/1080_only.sh
+++ bin/1080_only.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS1 --off --output DP1 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --off
+xrandr --output LVDS-1 --off --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output VGA-1 --off
